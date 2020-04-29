bb098754705bb88a4fb56f9583ca6f1c36bf2ab7
neilz <zhangnan3777@gmail.com>
2020-04-29 15:00:42 +0800

    多数据源配置

---
+++ chapter2/datasource-demo/src/main/java/neilz/spring/data/datasourcedemo/MultiDataSourceDemoApplication.java
@@ -0,0 +1,76 @@
+package neilz.spring.data.datasourcedemo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+/**
+ * exclude = {} 排除三个自动配置的类
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        JdbcTemplateAutoConfiguration.class})
+@Slf4j
+public class MultiDataSourceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiDataSourceDemoApplication.class, args);
+    }
+
+    @Bean
+    @ConfigurationProperties("foo.datasource")
+    public DataSourceProperties fooDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource fooDataSource() {
+        DataSourceProperties dataSourceProperties = fooDataSourceProperties();
+        log.info("foo datasource: {}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    /**
+     * @Bean 是说我这个方法的返回值当做一个Bean
+     * @Resource 是说我这个方法的参数要按照名字来注入其他的Bean
+     *
+     * @param fooDataSource
+     * @return
+     */
+    @Bean
+    @Resource
+    public PlatformTransactionManager fooTxManager(DataSource fooDataSource) {
+        return new DataSourceTransactionManager(fooDataSource);
+    }
+
+    @Bean
+    @ConfigurationProperties("bar.datasource")
+    public DataSourceProperties barDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource barDataSource() {
+        DataSourceProperties dataSourceProperties = barDataSourceProperties();
+        log.info("bar datasource: {}", dataSourceProperties.getUrl());
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    @Resource
+    public PlatformTransactionManager barTxManager(DataSource barDataSource) {
+        return new DataSourceTransactionManager(barDataSource);
+    }
+}
+
